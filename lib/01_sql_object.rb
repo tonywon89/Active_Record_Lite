0bf66a17d3493220f36c96bdea4433d3f9ef1050
Tony Won <tonywon89@gmail.com>
2016-03-25 11:17:17 -0700

    Finish initialize method in SQLObject

--- lib/01_sql_object.rb
+++ lib/01_sql_object.rb
@@ -5,10 +5,24 @@ require 'active_support/inflector'
 
 class SQLObject
   def self.columns
-    # ...
+    @columns ||= DBConnection.execute2(<<-SQL)
+      SELECT
+        *
+      FROM
+        #{table_name}
+    SQL
+    @columns[0].map(&:to_sym)
   end
 
   def self.finalize!
+    columns.each do |col|
+      define_method(col) do
+        attributes[col]
+      end
+      define_method("#{col}=") do |value|
+        attributes[col] = value
+      end
+    end
   end
 
   def self.table_name=(table_name)
@@ -32,11 +46,17 @@ class SQLObject
   end
 
   def initialize(params = {})
-    # ...
+    params.each do |attr_name, value|
+      unless self.class.columns.include?(attr_name)
+        raise "unknown attribute '#{attr_name}'"
+      end
+      attr_name = attr_name.to_sym
+      send("#{attr_name}=", value)
+    end
   end
 
   def attributes
-    # ...
+    @attributes ||= {}
   end
 
   def attribute_values
