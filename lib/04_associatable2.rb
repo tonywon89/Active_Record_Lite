a7b7fa3e4d29fc2cb14d4bce278fa52df8afd3ce
Tony Won <tonywon89@gmail.com>
2016-03-25 15:03:46 -0700

    Finish has_one_through and passed all specs

--- lib/04_associatable2.rb
+++ lib/04_associatable2.rb
@@ -5,6 +5,26 @@ module Associatable
   # Remember to go back to 04_associatable to write ::assoc_options
 
   def has_one_through(name, through_name, source_name)
-    
+
+    define_method(name) do
+      through_options = self.class.assoc_options[through_name]
+      source_options = through_options.model_class.assoc_options[source_name]
+
+      result = DBConnection.execute(<<-SQL, self.id)
+        SELECT
+          #{source_options.table_name}.*
+        FROM
+          #{through_options.table_name}
+        JOIN
+          #{source_options.table_name}
+        ON
+          #{through_options.table_name}.#{source_options.foreign_key} = #{source_options.table_name}.id
+        WHERE
+          #{through_options.table_name}.id = ?
+      SQL
+
+      source_options.model_class.new(result.first)
+    end
+
   end
 end
